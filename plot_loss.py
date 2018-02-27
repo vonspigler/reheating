c7c683e424486e1bb1e7f26ed31e79165522311b
vonspigler <stefano@spigler.net>
2018-02-27 17:31:51 +0100

    plot_loss.py: now it plots also a normalized plot (divided by the coldest curve)

--- plot_loss.py
+++ plot_loss.py
@@ -52,14 +52,12 @@ if len(cold_losses) == 0:
     print("Cold losses not found!")
     quit()
 
-#print(cold_losses)
-#quit()
 
 # -- Plot the loss data -- #
 
+
 _, (plt_left, plt_right) = plt.subplots(1, 2, sharey = True, figsize = (16.8, 10))
 plt.tight_layout()
-
 
 print("Plotting cold run...")
 lr = cold_losses[0]
@@ -83,6 +81,31 @@ plt_right.legend()
 
 #plt.show()
 with PdfPages(OUTPUT_DIR + '/all_losses.pdf') as pdf:
+    pdf.savefig()
+
+plt.close()
+
+
+# -- Plot the loss data (normalized) -- #
+
+
+plt.figure(figsize = (16.8, 10))
+plt.tight_layout()
+
+print("Plotting reheated losses (normalized)...")
+for reheated_losses in all_reheated_losses:
+    lr, bs = reheated_losses[0:2]
+    plt.plot(
+        [ t*lr for t, l in reheated_losses[-1] ], [ float(l)/l0 for (t, l), (_, l0) in zip(reheated_losses[-1], all_reheated_losses[0][-1]) ], '-',
+        label = "T={}, lr={}, bs={}".format(lr*bs, lr, bs)
+    )
+plt.yscale('log')
+plt.xscale('log')
+plt.xlabel('$\lambda$t')
+plt.legend()
+
+#plt.show()
+with PdfPages(OUTPUT_DIR + '/all_losses_normalized.pdf') as pdf:
     pdf.savefig()
 
 plt.close()
